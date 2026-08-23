9bea0c4e99ede61e155bdae38ecfc620abfe816c
Alessandro <galloalessandro77@gmail.com>
2026-08-24 00:12:31 +0200

    Added authentication messages

--- backend/src/constants/apiMessages.ts
+++ backend/src/constants/apiMessages.ts
@@ -10,4 +10,31 @@ export const apiMessages = {
     apiKeyNotConfigured: "Spoonacular API key not configured",
     invalidRecipeId: "The recipe could not be found",
   },
+  authentication: {
+    registration: {
+      unableToRegister: "Unable to register user",
+      invalidData: "Please use a valid email or password",
+      emailAlreadyUsed: "Email already in use",
+    },
+    login: {
+      loginFailed: "Login failed",
+      invalidCredentials: "Invalid credentials",
+      invalidData: "invalid email or password",
+      loggedIn: "Login successful",
+    },
+    logout: {
+      invalidCredentials: "Invalid credentials",
+      failedLogout: "Logout failed",
+      logoutSuccessful: "Logout successful",
+    },
+    refreshToken: {
+      invalid: "Invalid refresh token",
+      invalidOrExpired: "Invalid or expired refresh token",
+      unableToRefresh: "Unable to refresh access token",
+    },
+    middleware: {
+      required: "Authentication required",
+      invalid: "Invalid authorization",
+    },
+  },
 } as const;
