12b2ea5d97ff151367b5f274e46475fc16141622
Alessandro <galloalessandro77@gmail.com>
2026-08-25 16:31:50 +0200

    Add favorites routes

---
+++ backend/src/routes/favorites.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getFavoriteRecipesController,
+  addFavoriteRecipeController,
+  removeFavoriteRecipeController,
+} from "../controllers/favorites.controller";
+
+const router = Router();
+
+router.get("/", getFavoriteRecipesController);
+
+router.post("/:recipeId", addFavoriteRecipeController);
+
+router.delete("/:recipeId", removeFavoriteRecipeController);
+
+export default router;
