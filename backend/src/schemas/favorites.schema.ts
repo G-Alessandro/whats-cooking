e0f3ffa6037aa3e510a33124b1cea664828b7d15
Alessandro <galloalessandro77@gmail.com>
2026-09-05 00:18:34 +0200

    Add new favorite schema

--- backend/src/schemas/favorites.schema.ts
+++ backend/src/schemas/favorites.schema.ts
@@ -1,3 +1,11 @@
 import { z } from "zod";
 
-export const recipeIdParamSchema = z.coerce.number().int().positive();
+export const favoriteRecipeIdParamSchema = z.coerce.number().int().positive();
+
+export const newFavoriteRecipeSchema = z.object({
+  recipeName: z.string(),
+  recipeImage: z.string(),
+  recipeId: z.number(),
+});
+
+export type NewFavoriteRecipe = z.infer<typeof newFavoriteRecipeSchema>;
