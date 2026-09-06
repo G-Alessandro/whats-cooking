2ccb8b11668d56ee67f987479e4255aa0f066d19
Alessandro <galloalessandro77@gmail.com>
2026-09-06 14:49:30 +0200

    Add tests recipe mapper with favorites

--- backend/tests/recipes/recipes.mappers.test.ts
+++ backend/tests/recipes/recipes.mappers.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { mapRecipeInstructions } from "../../src/mappers/recipes.mapper";
+import { mapRecipesWithFavorites } from "../../src/mappers/recipes.mapper";
+import { RecipesListResult } from "../../src/schemas/recipes.schema";
+import { FavoriteRecipes } from "../../src/schemas/favorites.schema";
 
 describe("mapRecipeInstructions", () => {
   it("adds total equipment and total ingredients", () => {
@@ -186,6 +189,170 @@ describe("mapRecipeInstructions", () => {
 
   it("returns an empty array when there are no recipes", () => {
     const result = mapRecipeInstructions([]);
+
+    expect(result).toEqual([]);
+  });
+});
+
+describe("mapRecipesWithFavorites", () => {
+  it("mark a recipe as favorite when it is in the favorite recipes", () => {
+    const recipes = [
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image.jpg",
+      },
+    ];
+
+    const favoriteRecipes = [
+      {
+        recipeName: "Easy Chicken Tandoori",
+        recipeImage: "image.jpg",
+        recipeId: 641904,
+      },
+    ];
+
+    const result = mapRecipesWithFavorites(recipes, favoriteRecipes);
+
+    expect(result).toEqual([
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image.jpg",
+        isFavorite: true,
+      },
+    ]);
+  });
+
+  it("mark a recipe as not favorite when it is not in the favorite recipes", () => {
+    const recipes = [
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image.jpg",
+      },
+    ];
+
+    const favoriteRecipes = [
+      {
+        recipeName: "Italian Chicken With Pepper",
+        recipeImage: "image2.jpg",
+        recipeId: 648097,
+      },
+    ];
+
+    const result = mapRecipesWithFavorites(recipes, favoriteRecipes);
+
+    expect(result).toEqual([
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image.jpg",
+        isFavorite: false,
+      },
+    ]);
+  });
+
+  it("correctly mark multiple recipes", () => {
+    const recipes = [
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image1.jpg",
+      },
+      {
+        id: 648097,
+        title: "Italian Chicken With Pepper",
+        image: "image2.jpg",
+      },
+      {
+        id: 655620,
+        title: "Peperonata",
+        image: "image3.jpg",
+      },
+    ];
+
+    const favoriteRecipes = [
+      {
+        recipeName: "Easy Chicken Tandoori",
+        recipeImage: "image1.jpg",
+        recipeId: 641904,
+      },
+      {
+        recipeName: "Peperonata",
+        recipeImage: "image3.jpg",
+        recipeId: 655620,
+      },
+    ];
+
+    const result = mapRecipesWithFavorites(recipes, favoriteRecipes);
+
+    expect(result).toEqual([
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image1.jpg",
+        isFavorite: true,
+      },
+      {
+        id: 648097,
+        title: "Italian Chicken With Pepper",
+        image: "image2.jpg",
+        isFavorite: false,
+      },
+      {
+        id: 655620,
+        title: "Peperonata",
+        image: "image3.jpg",
+        isFavorite: true,
+      },
+    ]);
+  });
+
+  it("mark all recipes as not favorite when favorite recipes are empty", () => {
+    const recipes = [
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image1.jpg",
+      },
+      {
+        id: 648097,
+        title: "Italian Chicken With Pepper",
+        image: "image2.jpg",
+      },
+    ];
+
+    const favoriteRecipes: FavoriteRecipes = [];
+    const result = mapRecipesWithFavorites(recipes, favoriteRecipes);
+
+    expect(result).toEqual([
+      {
+        id: 641904,
+        title: "Easy Chicken Tandoori",
+        image: "image1.jpg",
+        isFavorite: false,
+      },
+      {
+        id: 648097,
+        title: "Italian Chicken With Pepper",
+        image: "image2.jpg",
+        isFavorite: false,
+      },
+    ]);
+  });
+
+  it("return an empty array when recipes are empty", () => {
+    const recipes: RecipesListResult = [];
+    const favoriteRecipes = [
+      {
+        recipeName: "Easy Chicken Tandoori",
+        recipeImage: "image1.jpg",
+        recipeId: 641904,
+      },
+    ];
+
+    const result = mapRecipesWithFavorites(recipes, favoriteRecipes);
 
     expect(result).toEqual([]);
   });
