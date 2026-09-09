df626a96723e1303b6e6e190246a915a1bc1e9d8
Alessandro <galloalessandro77@gmail.com>
2026-09-09 14:49:35 +0200

    Add recipes list schema

---
+++ mobile/src/schemas/recipes.schemas.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const recipeIngredientSchema = z.object({
+  id: z.number(),
+  image: z.string(),
+  name: z.string(),
+  amount: z.number(),
+  unit: z.string(),
+});
+
+export const recipeByIngredientsResultSchema = z.array(
+  z.object({
+    id: z.number(),
+    title: z.string(),
+    image: z.string(),
+    usedIngredientCount: z.number().optional(),
+    usedIngredients: z.array(recipeIngredientSchema).optional(),
+    missedIngredientCount: z.number().optional(),
+    missedIngredients: z.array(recipeIngredientSchema).optional(),
+    isFavorite: z.boolean().optional(),
+  }),
+);
+
+export type RecipesListResult = z.infer<typeof recipeByIngredientsResultSchema>;
