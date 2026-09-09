2c309f25c7ba9d07531db5fa54ce704cffc34179
Alessandro <galloalessandro77@gmail.com>
2026-09-09 14:48:01 +0200

    Add recipe provider

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,12 +1,15 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
+import { RecipeProvider } from "../context/RecipeContext";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
+      <RecipeProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </RecipeProvider>
     </AuthProvider>
   );
 }
