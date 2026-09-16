f0bd829e14519b4ccf4874004bb2ed34fc2bb41a
Alessandro <galloalessandro77@gmail.com>
2026-09-16 20:48:15 +0200

    Add component recipe list

---
+++ mobile/src/components/RecipeList.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, ScrollView, View, Text } from "react-native";
+import RecipeCard from "./RecipeCard";
+import { useRecipes } from "../context/RecipeContext";
+
+export default function RecipeList() {
+  const { recipesList } = useRecipes();
+
+  return (
+    <View>
+      <Text style={styles.title}>Recipes</Text>
+      <View style={styles.container}>
+        {recipesList.length > 0 && (
+          <ScrollView style={styles.listContainer}>
+            {recipesList.map((recipe) => {
+              return <RecipeCard key={recipe.id} recipe={recipe} />;
+            })}
+          </ScrollView>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingHorizontal: 12,
+  },
+
+  title: {
+    width: "100%",
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    paddingBottom: 15,
+    borderBottomColor: "green",
+    borderBottomWidth: 2,
+    marginTop: 50,
+    marginBottom: 30,
+  },
+
+  listContainer: { marginBottom: 280 },
+});
