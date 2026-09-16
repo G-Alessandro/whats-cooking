7669ae06a1243953be8de83e4b8652ebc86356a7
Alessandro <galloalessandro77@gmail.com>
2026-09-16 21:56:43 +0200

    Add global styles

---
+++ mobile/src/styles/global.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  background: "green",
+  header: "#196d13",
+  surface: "#276d22",
+  primary: "#ffad55",
+  text: "#ffffff",
+  textSecondary: "#ffffff",
+  alert: "#ff5252",
+};
